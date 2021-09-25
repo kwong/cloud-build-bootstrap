84adc775e3b0657539f0041636b007df45ded3db
Kangwei Wong <kangwei.wong@servian.com>
2021-09-25 12:49:24 +0800

    fixed formatting

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "sourcerepo_url" {
   description = "Source Repo created by the module"
-  value       = [for s in google_sourcerepo_repository.source_repo: s.url]
+  value       = [for s in google_sourcerepo_repository.source_repo : s.url]
 }
 
 output "cloudbuild_project_id" {
